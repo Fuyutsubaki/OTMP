0e4900ec767a1b1a986cb4ed3384fa66aaaa7b04
Fuyutsubaki <pushrunkey@gmail.com>
2014-08-17 19:56:16 +0900

    この行を消すとなぜか動かない

--- OTMP/main.cpp
+++ OTMP/main.cpp
@@ -1,21 +1,16 @@
+#include<iostream>
 #include<otmp.hpp>
 
-
-namespace otmp
-{
-	//cat
-	static_assert(std::is_same<cat_t<List<>>, List<>>::value, "");
-	static_assert(std::is_same<cat_t<List<List<char>, List<>, List<int, long>>>, List<char, int, long>>::value, "");
-
-	//filter
-	static_assert(std::is_same<filter_if_t<List<char, void, int>, lift<std::is_integral>>, List<char, int>>::value, "");
-
-
-
-	//all
-	static_assert(!all_t<List<char, void>, lift<std::is_integral>>::value, "");
-	static_assert(all_t<List<char, int>, lift<std::is_integral>>::value, "");
-}
+template<class T>
+struct in2
+	:otmp::in<T, otmp::Vector<int>>::type
+	//:otmp::Bool_<std::is_same<T,int>::value>
+{};
 
 int main()
-{}
+{
+	using namespace otmp;
+	using list = Vector<float, void, double>;
+	using R = map<list, lift<in2>>::type;//Ç±ÇÃçsÇè¡Ç∑Ç∆Ç»Ç∫Ç©ìÆÇ©Ç»Ç¢
+	static_assert(otmp::in<void, otmp::Vector<int>>::type::value, "");
+}
